966439a46d9a73940161b12f927cce43dacc413a
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-11-08 17:39:47 -0300

    Primer commit

---
+++ PAD-TFI/PAD.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PAD.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
