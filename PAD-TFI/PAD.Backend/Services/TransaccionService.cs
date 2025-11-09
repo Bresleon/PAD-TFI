f3ae83e26123ab8cbd03005e0ebed376f0072e38
ivanochoa229 <ivanochoa229@gmail.com>
2025-11-09 18:59:28 -0300

    agregando cambios

--- PAD-TFI/PAD.Backend/Services/TransaccionService.cs
+++ PAD-TFI/PAD.Backend/Services/TransaccionService.cs
@@ -14,12 +14,15 @@ public class TransaccionService
 
     public async Task<List<TransaccionDTO>> ObtenerPorRangoDeFechaAsync(DateTime desde, DateTime? hasta)
     {
-        DateTime fechaHasta = hasta.HasValue ? hasta.Value.Date.AddDays(1).AddSeconds(-1) : DateTime.Now;
-        DateTime fechaDesde = desde.Date;
-        
+        DateOnly fechaDesde = DateOnly.FromDateTime(desde);
+
+        DateOnly fechaHasta = hasta.HasValue
+            ? DateOnly.FromDateTime(hasta.Value)
+            : DateOnly.FromDateTime(DateTime.Today);
+
         var transaccionesQuery = _context.Transacciones
   
-            .Where(t => t.FechaTransaccion >= fechaDesde && t.FechaTransaccion <= fechaHasta)
+            .Where(t => t.Fecha >= fechaDesde && t.Fecha <= fechaHasta)
             
             
             .Include(t => t.TitularDestino)
@@ -35,7 +38,7 @@ public class TransaccionService
         var resultadoDTO = await transaccionesQuery.Select(t => new TransaccionDTO
         {
          
-            FechaTransaccion = t.FechaTransaccion,
+            FechaTransaccion = t.Fecha,
             CostoOperacion = t.Costo,
             TipoTransaccion = t.TipoTransaccion.ToString(), 
 
