13f5da7c6c77f5b8a84efd8c1b8a3b8d71ec249b
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-11-09 18:49:33 -0300

    Corrijo tabla transacciones, agregandole la patente y fecha de transacción

---
+++ PAD-TFI/PAD.Backend/Migrations/20251109214444_Modelo-Datos-Transaccion-Con-Patente-Y-Fecha.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PAD.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModeloDatosTransaccionConPatenteYFecha : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Fecha",
+                table: "transacciones",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<int>(
+                name: "PatenteId",
+                table: "transacciones",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "transacciones",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Fecha", "PatenteId" },
+                values: new object[] { new DateOnly(2023, 8, 21), 2 });
+
+            migrationBuilder.UpdateData(
+                table: "transacciones",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Fecha", "PatenteId" },
+                values: new object[] { new DateOnly(2024, 9, 23), 3 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transacciones_PatenteId",
+                table: "transacciones",
+                column: "PatenteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_transacciones_patentes_PatenteId",
+                table: "transacciones",
+                column: "PatenteId",
+                principalTable: "patentes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_transacciones_patentes_PatenteId",
+                table: "transacciones");
+
+            migrationBuilder.DropIndex(
+                name: "IX_transacciones_PatenteId",
+                table: "transacciones");
+
+            migrationBuilder.DropColumn(
+                name: "Fecha",
+                table: "transacciones");
+
+            migrationBuilder.DropColumn(
+                name: "PatenteId",
+                table: "transacciones");
+        }
+    }
+}
