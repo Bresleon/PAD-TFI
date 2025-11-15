b780b401753c39c322b8861b8fe2a401541de027
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-11-15 19:37:11 -0300

    Agrego endpoint para obtener transacciones por dni del titularDestino

--- PAD-TFI/PAD.Backend/Controllers/TransaccionController.cs
+++ PAD-TFI/PAD.Backend/Controllers/TransaccionController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using PAD.Backend.Dtos;
 using PAD.Backend.DTOs;
@@ -17,11 +18,12 @@ public class TransaccionesController : ControllerBase
 
     [HttpPost("generar-nueva-patente")]
     [ProducesResponseType(typeof(TransaccionDTO), StatusCodes.Status201Created)]  
-    public async Task<IActionResult> generarNuevaPatente(TransaccionAltaRequestDto request)
+    public async Task<IActionResult> GenerarNuevaPatente(TransaccionAltaRequestDto request)
     {
         var transaccion = await _transaccionService.GenerarNuevaPatenteAsync(request);
         return Ok(transaccion);
     }
+
     [HttpPost("transferir-patente")]
     [ProducesResponseType(typeof(TransaccionDTO), StatusCodes.Status201Created)]
     public async Task<IActionResult> TransferirPatente(TransaccionTransferenciaRequestDto request)
@@ -29,8 +31,6 @@ public class TransaccionesController : ControllerBase
         var transaccion = await _transaccionService.TransferirPatenteAsync(request);
         return Ok(transaccion);
     }
-
-
 
     [HttpGet("obtener-por-rango")]
     [ProducesResponseType(typeof(List<TransaccionResponseDTO>), StatusCodes.Status200OK)]
@@ -43,7 +43,22 @@ public class TransaccionesController : ControllerBase
 
         if (transacciones == null || !transacciones.Any())
         {
-            return NotFound("No se encontraron transacciones en el rango especificado.");
+            return BadRequest("No se encontraron transacciones en el rango especificado.");
+        }
+
+        return Ok(transacciones);
+    }
+
+    [HttpGet("obtener-por-dni")]
+    [ProducesResponseType(typeof(List<TransaccionResponseDTO>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetTransaccionesPorDni([FromQuery, Required] string dni)
+    {
+        var transacciones = await _transaccionService.ObtenerPorDni(dni);
+
+        if (transacciones == null || !transacciones.Any())
+        {
+            return BadRequest("No se encontraron transacciones para el dni ingresado.");
         }
 
         return Ok(transacciones);
