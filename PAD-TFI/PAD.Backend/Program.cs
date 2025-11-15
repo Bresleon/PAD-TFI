017d252bc0d95edd69e4acac011efc1fcf199db2
ivanochoa229 <ivanochoa229@gmail.com>
2025-11-15 13:38:47 -0300

    agregando configuracion en program.cs

--- PAD-TFI/PAD.Backend/Program.cs
+++ PAD-TFI/PAD.Backend/Program.cs
@@ -1,6 +1,8 @@
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using PAD.Backend.Data;
-using DotNetEnv;
+using PAD.Backend.Services;
+using PAD.Backend.ThirdPartyServiceCommunication.MercadoPago.Service;
 
 Env.Load();
 
@@ -8,11 +10,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 string renaperApiKey = builder.Configuration["RENAPER_API_KEY"]
                         ?? throw new InvalidOperationException("La variable de entorno 'RENAPER_API_KEY' no se ha encontrado.");
+string mercadoPagoAccessToken = builder.Configuration["MP_ACCESS_TOKEN"]
+                                ?? throw new InvalidOperationException("La variable de entorno 'MP_ACCESS_TOKEN' no se ha encontrado.");
 
 // Add services to the container.
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<TransaccionService>();
+builder.Services.AddScoped<TitularService>();
+builder.Services.AddScoped<PatenteService>();
+builder.Services.AddScoped<VehiculoService>();
+builder.Services.AddScoped<MercadoPagoService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,15 +31,28 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
 
-builder.Services.AddHttpClient("RenaperClient", client =>
+builder.Services.AddHttpClient<RenaperService>(client => 
 {
-    // Aqui pondremos la url que despliegue el grupo de RENAPER
     client.BaseAddress = new Uri("https://renaper-simulador.onrender.com/");
     client.DefaultRequestHeaders.Add("Accept", "application/json");
     client.DefaultRequestHeaders.Add("X-API-Key", renaperApiKey);
 });
 
-builder.Services.AddScoped<RenaperService>();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            // Hay que ver que puertos usa el front en local
+            policy.WithOrigins("http://localhost:4200", "http://localhost:3000")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -40,6 +64,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
