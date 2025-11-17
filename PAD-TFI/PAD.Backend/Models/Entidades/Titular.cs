1d034ebe697d17154bccc25f6b81eeab95ba64c4
Leon Breslauer <leonbreslauer.tuc@gmail.com>
2025-11-17 12:04:56 -0300

    Quito propiedad Marca en entidad Vehiculo, para así usar su Modelo al necesitar Marca

--- PAD-TFI/PAD.Backend/Models/Entidades/Titular.cs
+++ PAD-TFI/PAD.Backend/Models/Entidades/Titular.cs
@@ -9,4 +9,7 @@ public class Titular
     public string? Cuil { get; set; }
     public string Email { get; set; }
     public string Telefono { get; set; }
+
+    public ICollection<Patente> Patentes { get; set; } = new List<Patente>();
+    public ICollection<Transaccion> Transacciones { get; set;} = new List<Transaccion>();
 }
